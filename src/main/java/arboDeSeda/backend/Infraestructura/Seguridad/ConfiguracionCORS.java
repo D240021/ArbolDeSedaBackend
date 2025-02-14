3f2713714f451b6424702b36869159ff41d3a487
Daniel Barrantes <danielbarrantesq@gmail.com>
2025-02-14 14:44:45 -0600

    Se cambia ruta del CORS

--- src/main/java/arboDeSeda/backend/Infraestructura/Seguridad/ConfiguracionCORS.java
+++ src/main/java/arboDeSeda/backend/Infraestructura/Seguridad/ConfiguracionCORS.java
@@ -10,7 +10,7 @@ public class ConfiguracionCORS implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost")
+                .allowedOrigins("https://arboldesedabackend.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 
